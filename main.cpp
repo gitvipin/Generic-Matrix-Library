8c64b8ea5fa2027e3bf0c639a3ff651ca0a2e11a
GitVipin <sh.vipin@gmail.com>
2011-11-28 20:53:26 +0530

    moving matrix testing to matrix.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,7 @@
-#include "common.h"
-#include "matrix.h"
 
-int Data[] = {2,3,4,1,34,56,78,98,45,34,23,1,1,1,0,0,0,3,4,9,1,2,3,2,3,4,5,0};
-
+extern void TEST_MATRIX();
 
 int main (int argc , char *argv[]){
-
-    cout << "=========================================================" <<endl ;
-    cout << "============     Welcome to Matrix Library ==============" <<endl ;
-    cout << "=========================================================" <<endl ;
-
-    unsigned int size = sizeof Data/sizeof Data[0];
-	try {
-
-    Matrix<int>  m(4,5);
-    Matrix<int>  n(4,5);
-    Matrix<int>  o(4,5);
-
-    //m.GiveInput();// read from user , painful 
-    m.Read(&Data[5],size-5);
-    n.Read(&Data[7],size-7);
-
-    m.Print();
-    n.Print();
-
-    /*
-    o = m;
-    o = m+n;
-    o.Print();
-    */
-
-    (m+n).Print();
-    (m-n).Print();
-	}
-	catch(EXCEPTION_CODE e){
-		HandleError(e);
-	}
-	catch( Error & e){
-		HandleError(e);
-	}
-
-    return 0;
+	TEST_MATRIX();
 }
 
--- matrix.cpp
+++ matrix.cpp
@@ -1,3 +1,37 @@
 #include "common.h"
+#include "matrix.h"
+
+
+int Data[] = {2,3,4,1,34,56,78,98,45,34,23,1,1,1,0,0,0,3,4,9,1,2,3,2,3,4,5,0};
+
+void TEST_MATRIX(){	
+
+    cout << "=========================================================" <<endl ;
+    cout << "============     Welcome to Matrix Library ==============" <<endl ;
+    cout << "=========================================================" <<endl ;
+
+	unsigned int size = sizeof Data/sizeof Data[0];
+	try {
+		Matrix<int>  m(4,5);
+		Matrix<int>  n(4,5);
+		Matrix<int>  o(4,5);
+		
+		//m.GiveInput();// read from user , painful 
+		m.Read(&Data[5],size-5);
+		n.Read(&Data[7],size-7);
+		
+		m.Print();
+		n.Print();
+		
+		(m+n).Print();
+		(m-n).Print();
+	}
+	catch(EXCEPTION_CODE e){
+		HandleError(e);
+	}
+	catch( Error & e){
+		HandleError(e);
+	}
+}
 
 
